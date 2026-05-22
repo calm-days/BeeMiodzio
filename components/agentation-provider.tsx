9dfea705857d0a7ca0c8e8994b4316dee304f3e2
Roman Liukevich <raman.liukevich@ringieraxelspringer.pl>
2026-05-22 11:33:09 +0200

    disable agentation in production builds

--- components/agentation-provider.tsx
+++ components/agentation-provider.tsx
@@ -3,6 +3,10 @@
 import { Agentation } from "agentation";
 
 export function AgentationProvider() {
+  if (process.env.NODE_ENV === "production") {
+    return null;
+  }
+
   if (process.env.NEXT_PUBLIC_DISABLE_AGENTATION === "true") {
     return null;
   }
